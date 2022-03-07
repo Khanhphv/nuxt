b5477b12cb2c40bc21f0d42901c7fae6366192a7
Khanh Pham Viet <khanhpv2@rikkeisoft.com>
2022-03-07 18:59:18 +0700

    init

---
+++ components/custom-input/CustomInput.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+	b-form-group(:label="label")
+		b-form-input(:type="type" :name="name")
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from "nuxt-property-decorator"
+
+enum INPUT_TYPE {
+	text = "text",
+	email = "email"
+}
+
+@Component
+class CustomInput extends Vue {
+	@Prop() name!:string
+	@Prop() type!:INPUT_TYPE
+	@Prop() label!:string
+}
+
+export default CustomInput
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,62 @@
+<!-- Please remove this file from your project -->
+<template lang="pug" >
+b-container.container.login.d-flex.align-content-center
+  b-form(@submit="onSubmit")
+    CustomInput(name="name" type="text" label="Name")
+    b-form-group#email(label="Email" name="email")
+      b-form-input(v-model="form.email", placeholder="Enter email")
+    b-form-group#password(name="password", label="Password", )
+      b-form-input(v-model="form.password", placeholder="Enter Password")
+    b-button(type="submit") {{'Login'}} 
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+      },
+      show: true,
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event) {
+      event.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.name = "";
+      this.form.food = null;
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#__layout {
+  height: 100vh;
+  .login {
+    height: inherit;
+    width: 90%;
+    align-items: center;
+    justify-content: center;
+    form {
+      border: 1px solid;
+      padding: 10px;
+      width: 100%;
+      max-width: 500px;
+    }
+  }
+}
+</style>
